443ed23174f131c5b454f135ca0219b52c877ad4
annad96 <66536521+annad96@users.noreply.github.com>
2020-06-21 22:01:08 +0200

    Add files via upload

---
+++ Library/Book.cpp
@@ -0,0 +1,22 @@
+#include "Book.h"
+
+Book::Book(string author, string title, string keywords)
+{
+
+}
+/*
+string Book::author()
+{
+
+}
+
+string Book::title()
+{
+
+}
+
+string Book::keywords()
+{
+
+}
+*/
